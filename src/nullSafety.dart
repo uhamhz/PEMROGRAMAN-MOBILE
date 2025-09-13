5cb94c0bb06ce2c31edc13757d5fe0c3b2e05b04
uhamhz <ammarhafizh2404@gmail.com>
2025-09-13 15:18:15 +0700

    Reset Codelab03 to empty state

--- src/nullSafety.dart
+++
@@ -1,9 +0,0 @@
-void main() {
-  String? nama;
-  print("Nama: $nama");
-  if (nama != null) {
-    print("Panjang nama: ${nama.length}");
-  } else {
-    print("Nama belum diisi");
-  }
-}
--- src/umur.dart
+++
@@ -1,5 +0,0 @@
-void main() {
-  for (int i = 18; i > 8; i--) {
-    print("Nama saya adalah Ammar, sekarang berumur $i");
-  }
-}
