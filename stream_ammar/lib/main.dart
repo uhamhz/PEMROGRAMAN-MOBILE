ddb3c5b7f92d72fd5bc1c3ed6ba0f10f02962c79
uhamhz <ammarhafizh2404@gmail.com>
2025-11-13 16:06:56 +0700

    "W12: Jawaban Soal 4"

--- stream_ammar/lib/main.dart
+++ stream_ammar/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'stream.dart';
 
 void main() {
   runApp(const MyApp());
@@ -25,8 +26,29 @@ class StreamHomePage extends StatefulWidget {
 }
 
 class _StreamHomePageState extends State<StreamHomePage> {
+  Color bgColor = Colors.blueGrey;
+  late ColorStream colorStream;
+
+  @override
+  void initState() {
+    super.initState();
+    colorStream = ColorStream();
+    changeColor();
+  }
+
+  void changeColor() async {
+    await for (var eventColor in colorStream.getColors()) {
+      setState(() {
+        bgColor = eventColor;
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(title: const Text('Stream')),
+      body: Container(decoration: BoxDecoration(color: bgColor)),
+    );
   }
 }
